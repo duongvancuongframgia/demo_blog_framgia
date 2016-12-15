0b87a1bc04aa9cdc8e275ffee63f93ac6b573bd0
Duong Van Cuong <duong.van.cuong@framgia.com>
2016-12-15 13:19:39 +0700

    add Upload Image

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,17 +2,18 @@ class Entry < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
-  # mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true
-  # validate  :picture_size
+  validate  :picture_size
 
-  # private
-  # 	# Validates the size of an uploaded picture.
-  #   def picture_size
-  #     if picture.size > 5.megabytes
-  #       errors.add(:picture, "should be less than 5MB")
-  #     end
-  #   end
+  private
+
+  	# Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
